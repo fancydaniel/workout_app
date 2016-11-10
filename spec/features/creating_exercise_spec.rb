f3f8e4a74a4b970e2d509668484a58d639307302
Daniel Uribe <duribe8@gmail.com>
2016-11-09 21:14:04 -0800

    Fixed bug in Exercises validation

--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -21,7 +21,7 @@ RSpec.feature "Creating Exercise" do
 
     fill_in "Duration", with: 70
     fill_in "Workout Details", with: "Weight Lifting"
-    # fill_in "Activy date", with: "2016-10-30"
+    fill_in "Activity date", with: "2016-10-30"
     click_button "Create Exercise"
 
     expect(page).to have_content("Exercise has been created")
